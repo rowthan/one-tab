f4cb87546bc9be878fdb1b0dbb4e5a9796360192
ranronghua <ranronghuano1@163.com>
2019-02-03 12:58:28 +0800

    允许所有网页frame嵌入
    - 修改相应请求头 X-Frame-Options

---
+++ scripts/background.js
@@ -0,0 +1,20 @@
+var callback = function(details) {
+    var headers = details.responseHeaders;
+    for (var i = 0; i < headers.length; ++i) {
+        // 移除X-Frame-Options字段
+        if (headers[i].name === 'X-Frame-Options') {
+            headers.splice(i, 1);
+            break;
+        }
+    }
+    // 返回修改后的headers列表
+    return { responseHeaders: headers };
+};
+// 监听哪些内容
+var filter = {
+    urls: ["<all_urls>"]
+};
+// 额外的信息规范，可选的
+var extraInfoSpec = ["blocking", "responseHeaders"];
+/* 监听response headers接收事件*/
+chrome.webRequest.onHeadersReceived.addListener(callback, filter, extraInfoSpec);
